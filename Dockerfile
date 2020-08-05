b2094e6b4d31a0be547e19a3e4472d2775918c65
tristanlatr <tris.la.tr@gmail.com>
2020-08-05 14:21:34 -0400

    Ask if user want to build wpscan-analyzer from source in insall script

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,5 @@ RUN mkdir /wpscan-analyze/
 ADD . /wpscan-analyze/
 WORKDIR /wpscan-analyze
 RUN ls -alh
-RUN cargo build
 RUN cargo install --path .
 ENTRYPOINT ["wpscan-analyze"]
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# wpscan-analyze install script for MacOS and Linux. Only works for 64bits devices for now.
+# wpscan-analyze install script for MacOS and Linux.
 # This script will download, unzip and install binary file to /usr/local/bin/
 # Script requirement: wget
 # More infos https://github.com/lukaspustina/wpscan-analyze
@@ -11,53 +11,91 @@ binary_file="${install_to}/wpscan-analyze"
 # Fast fail if any errors
 set -e
 
-# Move to temp dir
+# Cd to temp and try to create/delete file
 cd /tmp/
+touch "wpscan-analayze-install-$(date)" && rm -f "wpscan-analayze-install-*"
 
-# Quit if not a 64 bits arch
-if [ ! "$(uname -m)" = x86_64 ]; then
-    echo "Unsupported architecture, only 64 bits are supported by this script. Please install Rust environment and build wpscan-analayze from source. Visit https://github.com/lukaspustina/wpscan-analyze for more infos."
+# Info banner
+echo
+echo
+echo "[INFO] wpscan-analyze install script for MacOS and Linux"
+echo
+echo
+
+#Cleaning git repo
+rm -rf wpscan-analyze 2>/dev/null || true
+# init repo
+if ! git clone https://github.com/lukaspustina/wpscan-analyze --quiet; then
+    echo "[ERROR] Github repo lukaspustina/wpscan-analyze is not accessible"
     exit 1
 fi
-
-# Determine version based on lastest git tag
-git clone https://github.com/lukaspustina/wpscan-analyze --quiet 
 cd wpscan-analyze
-version=`git tag | tail -1`
-cd ..
-rm -rf wpscan-analyze 2>/dev/null || true
 
-# Determine file name based on operating system and version
-filename=""
-if [ "$(uname)" = Linux ]; then
-    filename="wpscan-analyze-${version}-x86_64-unknown-linux-gnu"
+# Ask to build from source ?
+echo "[QUESTION] Do you want to build wpscan-analyze from source ?"
+read -p "[INFO] Answer No to download binary from github and copy it to ${install_to} [y/n] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    if ! which cargo; then
+        read -p "[QUESTION] Cargo is not detected. Do you want install Rust environment ? [y/n]" -n 1 -r
+        echo    # (optional) move to a new line
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+            source $HOME/.cargo/env
+        else
+            echo "Quitting"
+            exit 1
+        fi
+    fi
+    echo "[INFO] Builing..."
+    cargo install --path .
+    echo "[INFO] Uninstall with: 'cargo uninstall wpscan-analyze'"
 else
-    if [ "$(uname)" = Darwin ]; then
-        filename="wpscan-analyze-${version}-x86_64-apple-darwin"
-    else
-        echo "Unsupported operating system, please install Rust environment and build wpscan-analayze from source. Visit https://github.com/lukaspustina/wpscan-analyze for more infos."
+
+    # Quit if not a 64 bits arch
+    if [ ! "$(uname -m)" = x86_64 ]; then
+        echo "[ERROR] Unsupported architecture, only 64 bits are supported by this script. Please install Rust environment and build wpscan-analayze from source. Visit https://github.com/lukaspustina/wpscan-analyze for more infos."
         exit 1
     fi
+
+    # Determine version based on lastest git tag
+    version=`git tag | tail -1`
+    cd ..
+
+    # Determine file name based on operating system and version
+    filename=""
+    if [ "$(uname)" = Linux ]; then
+        filename="wpscan-analyze-${version}-x86_64-unknown-linux-gnu"
+    else
+        if [ "$(uname)" = Darwin ]; then
+            filename="wpscan-analyze-${version}-x86_64-apple-darwin"
+        else
+            echo "[ERROR] Unsupported operating system, please install Rust environment and build wpscan-analayze from source. Visit https://github.com/lukaspustina/wpscan-analyze for more infos."
+            exit 1
+        fi
+    fi
+    # Download
+    echo "[INFO] Downloading wpscan-analyze ${version} $(uname -m) $(uname) binary"
+
+    # CLeaning before downloading binary
+    rm "${filename}.gz" 2>/dev/null || true
+    rm "${filename}" 2>/dev/null || true
+
+    # Get binary file
+    wget --quiet "https://github.com/lukaspustina/wpscan-analyze/releases/download/${version}/${filename}.gz"
+
+    # Unzip it
+    gzip -d "${filename}.gz"
+
+
+    # Copy file
+    echo "[INFO] Copying binary from /tmp/${filename} to ${binary_file}"
+    rm "${binary_file}" 2>/dev/null || true
+    cp "${filename}" "${binary_file}"
+
+    # Make the file executable
+    chmod +x ${binary_file}
+    echo "[INFO] Uninstall with: 'rm ${binary_file}'"
 fi
-# Download
-echo "Downloading wpscan-analyze ${version} $(uname -m) $(uname) binary"
-
-# CLeaning before downloading binary
-rm "${filename}.gz" 2>/dev/null || true
-rm "${filename}" 2>/dev/null || true
-
-# Get binary file
-wget --quiet "https://github.com/lukaspustina/wpscan-analyze/releases/download/${version}/${filename}.gz"
-
-# Unzip it
-gzip -d "${filename}.gz"
-
-# Copy file
-echo "Copying binary to ${binary_file}"
-rm "${binary_file}" 2>/dev/null || true
-cp "${filename}" "${binary_file}"
-
-# Make the file executable
-chmod +x ${binary_file}
-
-echo "Success! Visit https://github.com/lukaspustina/wpscan-analyze for more informations."
+echo "[INFO] Success! You might have to reopen your terminal window to use 'wpscan-analyze'"
+echo "[INFO] Visit https://github.com/lukaspustina/wpscan-analyze for more informations"
