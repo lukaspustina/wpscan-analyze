8742aba63ab81ec927d415eb6bad52dc11320cf9
tristanlatr <tris.la.tr@gmail.com>
2020-08-06 16:30:40 -0400

    Re fix read command + Uninstall old version automatically

--- install.sh
+++ install.sh
@@ -16,21 +16,25 @@ cd /tmp/
 touch "wpscan-analayze-install-$(date)" && rm -f "wpscan-analayze-install-*"
 
 # Info banner
-echo
-echo
 echo "[INFO] wpscan-analyze install script for MacOS and Linux"
-echo
-echo
 
 # Determine latest version based on lastest git tag with github api
-version="v`curl --silent "https://api.github.com/repos/lukaspustina/wpscan-analyze/releases/latest" | grep tag_name | sed -E 's/.*"v(.*)",/\1/'`"
+raw_version=`curl --silent "https://api.github.com/repos/lukaspustina/wpscan-analyze/releases/latest" | grep tag_name | sed -E 's/.*"v(.*)",/\1/'`
+version="v${raw_version}"
 
+old_install=""
+if which -s wpscan-analyze; then
+    old_install=`which wpscan-analyze`
+    echo "[INFO] wpscan-analyze in already installed: ${old_install} (`wpscan-analyze --version`)"
+    echo "[INFO] The script will remove this installation"
+fi
+
+echo "[INFO] Latest version is ${raw_version}"
 # Ask to build from source ?
 echo "[QUESTION] Do you want to build latest wpscan-analyze version from source ?"
-echo "[INFO] Answer No to download binary from github and copy it to ${install_to} [y/n]"
-read continue
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+echo "[QUESTION] Answer No to download binary from github and copy it to ${install_to} [y/n/cancel]"
+read 
+if [ "$REPLY" = "y" ] || [ "$REPLY" = "yes" ] || [ "$REPLY" = "Y" ] || [ "$REPLY" = "Yes" ]; then
     #Cleaning git repo
     rm -rf wpscan-analyze
     # init repo
@@ -43,60 +47,85 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     git checkout ${version}
     if ! which cargo; then
         echo "[QUESTION] Cargo is not detected. Do you want install Rust environment? [y/n]"
-        read continue
-        echo    # (optional) move to a new line
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
+        read
+        if [ "$REPLY" = "y" ] || [ "$REPLY" = "yes" ] || [ "$REPLY" = "Y" ] || [ "$REPLY" = "Yes" ]; then
             curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
             source $HOME/.cargo/env
         else
-            echo "Quitting"
+            echo "Canceled"
             exit 1
         fi
     fi
+
+    if [ ! -z "${old_install}" ]; then
+        echo "[INFO] Removing old install ${old_install}"
+        if ! cargo uninstall wpscan-analyze; then
+            rm -rf "${old_install}"
+        fi
+    fi
+    
     echo "[INFO] Building..."
     cargo install --path .
     echo "[INFO] Uninstall with: 'cargo uninstall wpscan-analyze'"
-else
 
-    # Quit if not a 64 bits arch
-    if [ ! "$(uname -m)" = x86_64 ]; then
-        echo "[ERROR] Unsupported architecture, only 64 bits are supported by this script. Please install Rust environment and build wpscan-analayze from source. Visit https://github.com/lukaspustina/wpscan-analyze for more infos."
+else
+    if [ "$REPLY" = "n" ] || [ "$REPLY" = "no" ] || [ "$REPLY" = "N" ] || [ "$REPLY" = "No" ]; then
+        # Quit if not a 64 bits arch
+        if [ ! "$(uname -m)" = x86_64 ]; then
+            echo "[ERROR] Unsupported architecture, only 64 bits are supported by this script. Please install Rust environment and build wpscan-analayze from source. Visit https://github.com/lukaspustina/wpscan-analyze for more infos."
+            exit 1
+        fi
+
+        # Determine file name based on operating system and version
+        filename=""
+        if [ "$(uname)" = Linux ]; then
+            filename="wpscan-analyze-${version}-x86_64-unknown-linux-gnu"
+        else
+            if [ "$(uname)" = Darwin ]; then
+                filename="wpscan-analyze-${version}-x86_64-apple-darwin"
+            else
+                echo "[ERROR] Unsupported operating system, please install Rust environment and build wpscan-analayze from source. Visit https://github.com/lukaspustina/wpscan-analyze for more infos."
+                exit 1
+            fi
+        fi
+        # Download
+        echo "[INFO] Downloading wpscan-analyze ${version} $(uname -m) $(uname) binary"
+
+        # CLeaning before downloading binary
+        rm "${filename}.gz" 2>/dev/null || true
+        rm "${filename}" 2>/dev/null || true
+
+        # Get binary file
+
+        if ! wget "https://github.com/lukaspustina/wpscan-analyze/releases/download/${version}/${filename}.gz"; then
+            echo "[ERROR] Make sure 'wget' is installed on your system and your have internet"
+        fi
+
+        # Unzip it
+        gzip -d "${filename}.gz"
+
+        if [ ! -z "${old_install}" ]; then
+            echo "[INFO] Removing old install ${old_install}"
+            if ! cargo uninstall wpscan-analyze; then
+                rm -rf "${old_install}"
+            fi
+        fi
+
+        # Copy file
+        echo "[INFO] Copying binary from /tmp/${filename} to ${binary_file}"
+        rm "${binary_file}" 2>/dev/null || true
+        cp "${filename}" "${binary_file}"
+
+        # Make the file executable
+        chmod +x ${binary_file}
+        echo "[INFO] Uninstall with: 'rm ${binary_file}'"
+    
+    else
+        echo "Canceled"
         exit 1
     fi
 
-    # Determine file name based on operating system and version
-    filename=""
-    if [ "$(uname)" = Linux ]; then
-        filename="wpscan-analyze-${version}-x86_64-unknown-linux-gnu"
-    else
-        if [ "$(uname)" = Darwin ]; then
-            filename="wpscan-analyze-${version}-x86_64-apple-darwin"
-        else
-            echo "[ERROR] Unsupported operating system, please install Rust environment and build wpscan-analayze from source. Visit https://github.com/lukaspustina/wpscan-analyze for more infos."
-            exit 1
-        fi
-    fi
-    # Download
-    echo "[INFO] Downloading wpscan-analyze ${version} $(uname -m) $(uname) binary"
-
-    # CLeaning before downloading binary
-    rm "${filename}.gz" 2>/dev/null || true
-    rm "${filename}" 2>/dev/null || true
-
-    # Get binary file
-    wget "https://github.com/lukaspustina/wpscan-analyze/releases/download/${version}/${filename}.gz"
-
-    # Unzip it
-    gzip -d "${filename}.gz"
-
-    # Copy file
-    echo "[INFO] Copying binary from /tmp/${filename} to ${binary_file}"
-    rm "${binary_file}" 2>/dev/null || true
-    cp "${filename}" "${binary_file}"
-
-    # Make the file executable
-    chmod +x ${binary_file}
-    echo "[INFO] Uninstall with: 'rm ${binary_file}'"
 fi
+
 echo "[INFO] Success! You might have to reopen your terminal window to use 'wpscan-analyze'"
 echo "[INFO] Visit https://github.com/lukaspustina/wpscan-analyze for more informations"
